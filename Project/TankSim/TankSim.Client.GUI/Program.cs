e57fc9e0076df31963aaece7bb084f0c46612911
aual1780 <aual1780@colorado.edu>
2020-10-04 14:48:41 -0600

    Add gui project Add csproj files

---
+++ Project/TankSim/TankSim.Client.GUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TankSim.Client.GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
