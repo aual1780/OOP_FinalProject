158049b4e7954ae619817e37aef7429eea634aba
aual1780 <aual1780@colorado.edu>
2020-10-04 20:55:18 -0600

    Rename unity project

---
+++ Project/TankSim/TankSim.GameHost.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TankSim.GameHost.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
