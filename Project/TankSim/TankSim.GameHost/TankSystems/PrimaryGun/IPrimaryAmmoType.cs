6ab1558f5bd6330f0b0f5ac4781b501d1732fe1a
aual1780 <aual1780@colorado.edu>
2020-10-29 23:12:36 -0600

    add ammo type

---
+++ Project/TankSim/TankSim.GameHost/TankSystems/PrimaryGun/IPrimaryAmmoType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TankSim.GameHost.TankSystems
+{
+    /// <summary>
+    /// Interface representing ammo types for the primary gun
+    /// </summary>
+    public interface IPrimaryAmmoType
+    {
+    }
+}
