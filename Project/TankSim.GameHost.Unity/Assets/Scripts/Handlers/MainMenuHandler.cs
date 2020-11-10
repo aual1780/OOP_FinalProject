a766b36b172f4bbc07a6f704ec793599e4d9f3ec
Ian-Meadows <iame5815@colorado.edu>
2020-11-09 18:19:41 -0700

    Scenes and handler Scripts have been added

---
+++ Project/TankSim.GameHost.Unity/Assets/Scripts/Handlers/MainMenuHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+
+    public InputField gameNameIF;
+    public InputField playerAmountIF;
+
+    private GameController gc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gc = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void CreateLobbyClicked()
+    {
+        gc.CreateLobby(1, "yeet");
+    }
+
+    public void QuitClicked()
+    {
+        Application.Quit();
+    }
+}
