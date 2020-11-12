88ff2fbc48d0faf78b2ebdec3a53751501c24790
aalbert <aalbert@tipconsulting.io>
2020-11-11 23:00:31 -0700

    bump deps

--- Project/TankSim/TankSim.Client.GUI/LocalPublish.bat
+++ Project/TankSim/TankSim.Client.GUI/LocalPublish.bat
@@ -1,4 +1,4 @@
-dotnet publish -c Release -r win-x64 /p:PublishSingleFile=True /p:PublishTrimmed=True /p:PublishReadyToRun=True /p:Self-Contained=True
-dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=True /p:PublishTrimmed=True /p:PublishReadyToRun=True /p:Self-Contained=True
+dotnet publish -c Release -f net5.0-windows -r win-x64 /p:PublishSingleFile=True /p:PublishTrimmed=True /p:PublishReadyToRun=True /p:Self-Contained=True
+dotnet publish -c Release -f net5.0-windows -r win10-x64 /p:PublishSingleFile=True /p:PublishTrimmed=True /p:PublishReadyToRun=True /p:Self-Contained=True
 
 echo done
